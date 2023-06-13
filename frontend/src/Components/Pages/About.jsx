995b06621a7a0246dde35ad83114e41abccb1e01
bleonjusufi <bjusufi7@gmail.com>
2023-06-13 16:00:38 +0200

    Added Header Component to All Pages

--- frontend/src/Components/Pages/About.jsx
+++ frontend/src/Components/Pages/About.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 function About() {
-    <div>
-        Hello World
-    </div>
+    return (
+        <Header />
+    )
 }
 
 export default About
--- frontend/src/Components/Pages/Blog.jsx
+++ frontend/src/Components/Pages/Blog.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 function Blog() {
-    <div>
-        Hello World
-    </div>
+    return (
+        <Header />
+    )
 }
 
 export default Blog
