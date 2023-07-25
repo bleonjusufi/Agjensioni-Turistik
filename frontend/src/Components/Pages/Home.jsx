86bd403922eca6ef92e432153ba6eb1905e8f68f
LorenaaTr <lorenatroshupa@gmail.com>
2023-07-25 19:37:39 +0200

    .

--- frontend/src/Components/Pages/Home.jsx
+++ frontend/src/Components/Pages/Home.jsx
@@ -7,11 +7,7 @@ import '../CSS/Home.css';
 function Home() {
     return(
        <>
-        <Header />
-       
-        
-               
-                
+        <Header />   
             
        </>
     )
