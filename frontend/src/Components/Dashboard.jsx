d8ff4429ffdfd04c21e001773f526d6e612553ec
LorenaaTr <lorenatroshupa@gmail.com>
2023-10-22 21:13:24 +0200

    .

--- frontend/src/Components/Dashboard.jsx
+++ frontend/src/Components/Dashboard.jsx
@@ -1,21 +1,13 @@
 import './CSS/dashboard.css';
-import { Space } from "antd";
-import DashHeader from "../Components/dashHeader";
-import DashSidebar from "../Components/dashSidebar";
-import DashContent from "../Components/dashContent";
-import DashFooter from "../Components/dashFooter";
+
+
 
 
 function Dashboard() {
     return (
         <>
         <div className="dash-container">
-            <DashHeader />
-            <Space className='SideMenuAndPageContent'>
-                <DashSidebar></DashSidebar>
-                <DashContent></DashContent>
-            </Space>
-            <DashFooter/>
+            
         </div>
         
         </>
