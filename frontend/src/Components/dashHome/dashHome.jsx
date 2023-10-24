5be8532360fd09ad062ad1a803aa8cb9753cf931
LorenaaTr <lorenatroshupa@gmail.com>
2023-10-24 14:18:35 +0200

    dashboard Home Updates

--- frontend/src/Components/dashHome/dashHome.jsx
+++ frontend/src/Components/dashHome/dashHome.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import '../CSS/dashHome.css';
-//import FeaturedInfoo from './FeaturedInfo/featuredInfo';
 
 
 export default function dashHome() {
   return (
     <div className='dashHome'>
-    
+       <div className="card-container5">
+      <div className="card5">
+        <h2>Users</h2>
+        <p>Number of Users</p>
+      </div>
+      <div className="card5">
+        <h2>Bookings</h2>
+        <p>Number of Bookings</p>
+      </div>
+      <div className="card5">
+        <h2>Employees</h2>
+        <p>Number of Employees</p>
+      </div>
+    </div>
+
+    <div className="widgetContainer">
+       <div className="widgeSm">
+         <span className="widgeTittle">New Joined Members</span>
+       </div>
+
+       <div className="widgeLg">
+       <span className="widgeTittle">Latest Bookings</span>
+       </div>
+    </div>
     </div>
   )
 }
