4aad5c0b9b07d0a26290bde9f96e8dcc3ffc9a7d
bleonjusufi <bjusufi7@gmail.com>
2023-11-01 16:26:53 +0100

    Added MongoDB Connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const mysql = require('mysql');
+const mongoose = require('mongoose');
 const app = express();
 
+const url = 'mongodb+srv://PrTravelAgency:BleonLorena@travelagency.eg9c6mq.mongodb.net/?retryWrites=true&w=majority'
+
+async function connect() {
+    try{
+        await mongoose.connect(url)
+        console.log("Connected to MongoDB");
+    }catch (error) {
+        console.error(error);
+    }
+}
+
+connect();
 
 app.listen(8080, () => {
     console.log("Server running on port 8080")
